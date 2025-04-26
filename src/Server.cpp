da13ab681d584b08a0c5a27809faba336c3bd521
GenericP3rson <genericp3rson@gmail.com>
2025-04-26 16:43:25 -0400

    v1 of server

---
+++ src/Server.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <string>
+#include <hash_map>
+#include <vector>
+#include <memory>
+
+// Server which returns the files!
+class Server {
+public:
+    // clients request a file from the coordinator. If it doesn't exist, hash the filename and put it into the corresponding server 
+    string requestFile(std::string filename);
+
+private:
+    // Map of [file] -> server_data
+    std::map<std::string, string> file_to_data;
+}
+
+string Server::requestFile(std::string filename) {
+    for (const auto& [file, data] : file_to_data) {
+        if (file == filename) {
+            return data;
+        }
+    }
+
+    file_to_data[filename] = "";
+
+    return file_to_data[filename];
+
+}
+
+
+
