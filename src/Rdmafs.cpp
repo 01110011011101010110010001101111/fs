394bc614c627cc11dfdfc217bb97ef21c60511a0
Alex Hu <superalexanderhu@gmail.com>
2025-05-01 23:56:23 -0400

    add more functions

--- src/Rdmafs.cpp
+++ src/Rdmafs.cpp
@@ -30,6 +30,18 @@ int rdmafs_getattr(const char* path, struct stat* stbuf, fuse_file_info*) {
     return 0;
 }
 
+int rdmafs_access(const char *path, int mask) {
+	if (access(path, mask) == -1) return -errno;
+	return 0;
+}
+
+int rdmafs_readlink(const char *path, char *buf, size_t size) {
+    int res;
+	if ( (res = readlink(path, buf, size - 1)) == -1) return -errno;
+	buf[res] = '\0';
+	return 0;
+}
+
 int rdmafs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     off_t offset, struct fuse_file_info *fi,
     enum fuse_readdir_flags flags) {
@@ -49,6 +61,63 @@ int rdmafs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     }
     closedir(dp);
     return 0;
+}
+
+int mknod_wrapper(int dirfd, const char *path, const char *link,
+	int mode, dev_t rdev) {
+	int res;
+
+	if (S_ISREG(mode)) {
+		res = openat(dirfd, path, O_CREAT | O_EXCL | O_WRONLY, mode);
+		if (res >= 0)
+			res = close(res);
+	} else if (S_ISDIR(mode)) {
+		res = mkdirat(dirfd, path, mode);
+	} else if (S_ISLNK(mode) && link != NULL) {
+		res = symlinkat(link, dirfd, path);
+	} else if (S_ISFIFO(mode)) {
+		res = mkfifoat(dirfd, path, mode);
+#ifdef __FreeBSD__
+	} else if (S_ISSOCK(mode)) {
+		struct sockaddr_un su;
+		int fd;
+
+		if (strlen(path) >= sizeof(su.sun_path)) {
+			errno = ENAMETOOLONG;
+			return -1;
+		}
+		fd = socket(AF_UNIX, SOCK_STREAM, 0);
+		if (fd >= 0) {
+			/*
+			 * We must bind the socket to the underlying file
+			 * system to create the socket file, even though
+			 * we'll never listen on this socket.
+			 */
+			su.sun_family = AF_UNIX;
+			strncpy(su.sun_path, path, sizeof(su.sun_path));
+			res = bindat(dirfd, fd, (struct sockaddr*)&su,
+				sizeof(su));
+			if (res == 0)
+				close(fd);
+		} else {
+			res = -1;
+		}
+#endif
+	} else {
+		res = mknodat(dirfd, path, mode, rdev);
+	}
+
+	return res;
+}
+
+int rdmafs_mknod(const char *path, mode_t mode, dev_t rdev) {
+	int res;
+
+	res = mknod_wrapper(AT_FDCWD, path, NULL, mode, rdev);
+	if (res == -1)
+		return -errno;
+
+	return 0;
 }
 
 int rdmafs_open(const char *path, struct fuse_file_info *fi) {
@@ -114,14 +183,90 @@ int rdmafs_write(const char *path, const char *buf, size_t size,
     return res;
 }
 
+int rdmafs_mkdir(const char *path, mode_t mode) {
+	if (mkdir(path, mode) == -1) return -errno;
+	return 0;
+}
+
+int rdmafs_unlink(const char *path) {
+	if (unlink(path) == -1) return -errno;
+	return 0;
+}
+
+int rdmafs_rmdir(const char *path) {
+	if (rmdir(path) == -1) return -errno;
+	return 0;
+}
+
+int rdmafs_symlink(const char *from, const char *to) {
+	if (symlink(from, to) == -1) return -errno;
+	return 0;
+}
+
+int rdmafs_rename(const char *from, const char *to, unsigned int flags) {
+	if (flags) return -EINVAL;
+	if (rename(from, to) == -1) return -errno;
+	return 0;
+}
+
+int rdmafs_link(const char *from, const char *to) {
+	if (link(from, to) == -1) return -errno;
+	return 0;
+}
+
+int rdmafs_chmod(const char *path, mode_t mode,
+		     struct fuse_file_info*) {
+	if (chmod(path, mode) == -1) return -errno;
+	return 0;
+}
+
+int rdmafs_chown(const char *path, uid_t uid, gid_t gid,
+		     struct fuse_file_info*) {
+	if (lchown(path, uid, gid) == -1) return -errno;
+	return 0;
+}
+
+int rdmafs_truncate(const char *path, off_t size,
+			struct fuse_file_info *fi) {
+	int res;
+	if (fi) res = ftruncate(fi->fh, size);
+	else res = truncate(path, size);
+
+	if (res == -1) return -errno;
+	return 0;
+}
+
+int rdmafs_release(const char *path, struct fuse_file_info *fi) {
+	close(fi->fh);
+	return 0;
+}
+
+int rdmafs_statfs(const char *path, struct statvfs *stbuf) {
+	if (statvfs(path, stbuf) == -1) return -errno;
+	return 0;
+}
+
 int main(int argc, char* argv[]) {
     fuse_operations oper {};
     oper.init = rdmafs_init;
     oper.getattr = rdmafs_getattr;
+    oper.access = rdmafs_access;
+    oper.readlink = rdmafs_readlink;
     oper.readdir = rdmafs_readdir;
     oper.open = rdmafs_open;
     oper.create = rdmafs_create;
     oper.read = rdmafs_read;
     oper.write = rdmafs_write;
+    oper.mkdir = rdmafs_mkdir;
+    oper.unlink = rdmafs_unlink;
+    oper.rmdir = rdmafs_rmdir;
+    oper.symlink = rdmafs_symlink;
+    oper.rename = rdmafs_rename;
+    oper.link = rdmafs_link;
+    oper.chmod = rdmafs_chmod;
+    oper.chown = rdmafs_chown;
+    oper.truncate = rdmafs_truncate;
+    oper.statfs = rdmafs_statfs;
+    oper.release = rdmafs_release;
     return fuse_main(argc, argv, &oper, nullptr);
 }
